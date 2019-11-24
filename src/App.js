89df04a2eca1df48c8f880bfdf45010ee8dff9d6
Tetsushi Kawarabata <tetsushi.kawarabata@cluex.co.jp>
2019-11-24 23:27:46 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {
+  Component
+} from "react";
 
-function App() {
-  return <div><h1>Hello, world!</h1></div>
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="button" onClick = {() => {console.log("i am clicked")}} />
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
